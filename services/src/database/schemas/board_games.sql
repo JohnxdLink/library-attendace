860b7d328cf307a1b54c62940c4c8cba8648f08c
JohnxdLink <johnxdlink@gmail.com>
2026-08-25 12:54:03 +0800

    feat: Add board games for attendance.

---
+++ services/src/database/schemas/board_games.sql
@@ -0,0 +1,17 @@
+CREATE TABLE board_games (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    game_name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NULL,
+    quantity INT UNSIGNED NOT NULL DEFAULT 1,
+    status ENUM(
+        'available',
+        'unavailable'
+    ) NOT NULL DEFAULT 'available',
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id),
+    UNIQUE KEY uq_board_games_game_name (game_name)
+) ENGINE=InnoDB
+  DEFAULT CHARSET=utf8mb4
+  COLLATE=utf8mb4_unicode_ci;
